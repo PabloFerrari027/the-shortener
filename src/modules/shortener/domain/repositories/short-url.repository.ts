a52dc0243c87c588b5688acff5ca5e6b0ba561a6
Pablo Ferrari <pabloferraricaliari@gmail.com>
2025-11-26 13:28:41 -0300

    fix: improve ShortUrlRepository interface type safety

--- src/modules/shortener/domain/repositories/short-url.repository.ts
+++ src/modules/shortener/domain/repositories/short-url.repository.ts
@@ -3,13 +3,13 @@ import { ShortUrl, ShortUrlProps } from '../entities/short-url.entity';
 import { ListingResponse } from 'src/shared/types/listing-response.type';
 
 export interface ShortUrlRepository {
-  create(shortUrl: ShortUrl): Promise<void>;
-  findByHash(hash: string): Promise<ShortUrl | null>;
-  findById(id: string): Promise<ShortUrl | null>;
-  list(
+  create: (shortUrl: ShortUrl) => Promise<void>;
+  findByHash: (hash: string) => Promise<ShortUrl | null>;
+  findById: (id: string) => Promise<ShortUrl | null>;
+  list: (
     options?: PaginationOptions<keyof ShortUrlProps>,
-  ): Promise<ListingResponse<ShortUrl>>;
-  update(shortUrl: ShortUrl): Promise<void>;
-  count(): Promise<number>;
-  delete(id: string): Promise<void>;
+  ) => Promise<ListingResponse<ShortUrl>>;
+  update: (shortUrl: ShortUrl) => Promise<void>;
+  count: () => Promise<number>;
+  delete: (id: string) => Promise<void>;
 }
