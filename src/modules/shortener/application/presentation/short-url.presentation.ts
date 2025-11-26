a099c6489e8d716623111d02b790a374d003974b
Pablo Ferrari <pabloferraricaliari@gmail.com>
2025-11-26 09:55:25 -0300

    feat: add method overloading to ShortUrl presentation layer

--- src/modules/shortener/application/presentation/short-url.presentation.ts
+++ src/modules/shortener/application/presentation/short-url.presentation.ts
@@ -5,13 +5,40 @@ export type ToControllerOut = ShortUrlJSON<'SNAKE_CASE'> & {
   short_url: string;
 };
 
-export class ShortUrlPresentation {
-  static toController(shortUrl: ShortUrl): ToControllerOut {
-    const output = {
-      ...shortUrl.toJSON('SNAKE_CASE'),
-      short_url: `${Env.APP_URL}/${shortUrl.hash}`,
-    };
+export type ToControllerListOut = {
+  data: ToControllerOut[];
+  total_pages: number;
+  current_page?: number;
+};
 
-    return output;
+export class ShortUrlPresentation {
+  static toController(shortUrl: ShortUrl): ToControllerOut;
+
+  static toController(
+    shortUrls: ShortUrl[],
+    totalPages: number,
+    currentPage: number,
+  ): ToControllerListOut;
+
+  static toController(
+    input: ShortUrl | ShortUrl[],
+    totalPages?: number,
+    currentPage?: number,
+  ): ToControllerOut | ToControllerListOut {
+    if (Array.isArray(input)) {
+      return {
+        total_pages: totalPages!,
+        current_page: currentPage!,
+        data: input.map((shortUrl) => ({
+          ...shortUrl.toJSON('SNAKE_CASE'),
+          short_url: `${Env.APP_URL}/${shortUrl.hash}`,
+        })),
+      };
+    }
+
+    return {
+      ...input.toJSON('SNAKE_CASE'),
+      short_url: `${Env.APP_URL}/${input.hash}`,
+    };
   }
 }
