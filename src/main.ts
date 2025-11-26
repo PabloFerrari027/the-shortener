34f2340653de96d0b7a5dc7db05d46786eca55ab
Pablo Ferrari <pabloferraricaliari@gmail.com>
2025-11-26 10:45:09 -0300

    feat: add global validation pipe configuration

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,19 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { Env } from './shared/env';
 import { GlobalExceptionFilter } from './shared/infra/http/filter/global-exeption.filter';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalFilters(new GlobalExceptionFilter());
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    }),
+  );
+
   await app.listen(Env.PORT);
 }
 bootstrap();
