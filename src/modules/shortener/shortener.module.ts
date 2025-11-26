7a4a097e4a4b0450d2264013b876d226394e2cbe
Pablo Ferrari <pabloferraricaliari@gmail.com>
2025-11-26 10:46:33 -0300

    feat: register ListShortnerUrls service in module

--- src/modules/shortener/shortener.module.ts
+++ src/modules/shortener/shortener.module.ts
@@ -2,9 +2,14 @@ import { Module } from '@nestjs/common';
 import { ShortenerController } from './application/shortener.controller';
 import { CreateShortUrlService } from './application/services/create-short-url.service';
 import { HandleShortUrlService } from './application/services/handle-short-url.service';
+import { ListShortnerUrlsService } from './application/services/list-shortner-urls.service';
 
 @Module({
   controllers: [ShortenerController],
-  providers: [CreateShortUrlService, HandleShortUrlService],
+  providers: [
+    CreateShortUrlService,
+    HandleShortUrlService,
+    ListShortnerUrlsService,
+  ],
 })
 export class ShortenerModule {}
