d3d20dc9cbf6b9a89a3685cf9501be24ba127a49
Pablo Ferrari <pabloferraricaliari@gmail.com>
2025-11-29 20:21:29 -0300

    refactor: rename and improve role guard

--- src/modules/auth/infra/guards/admin.guard.ts
+++ src/modules/auth/infra/guards/admin.guard.ts
@@ -5,11 +5,11 @@ import {
   Inject,
 } from '@nestjs/common';
 import { Request } from 'express';
-import type { SessionsRepository } from '../../domain/repositories/sessions.repository';
 import { UnauthorizedError } from '../../domain/erros/unauthorized.error';
-import type { EncodingPort } from '@/shared/ports/encoding.port';
-import type { UsersRepository } from '@/modules/users/domain/repositories/users.repository';
 import { UserRole } from '@/modules/users/domain/entities/user.entity';
+import type { UsersRepository } from '@/modules/users/domain/repositories/users.repository';
+import type { EncodingPort } from '@/shared/ports/encoding.port';
+import type { SessionsRepository } from '../../domain/repositories/sessions.repository';
 
 interface Payload {
   sessionId: string;
@@ -21,10 +21,10 @@ export class AdminGuard implements CanActivate {
   constructor(
     @Inject('EncodingPort')
     private readonly encodingPort: EncodingPort,
-    @Inject()
-    private readonly sessionsRepository: SessionsRepository,
-    @Inject()
+    @Inject('UsersRepository')
     private readonly usersRepository: UsersRepository,
+    @Inject('SessionsRepository')
+    private readonly sessionsRepository: SessionsRepository,
   ) {}
 
   private extractTokenFromHeader(request: Request): string | undefined {
@@ -50,10 +50,6 @@ export class AdminGuard implements CanActivate {
       const session = await this.sessionsRepository.findById(payload.sessionId);
 
       if (!session) throw new UnauthorizedError();
-
-      if (session.closedAt || !session.validatedAt) {
-        throw new UnauthorizedError();
-      }
 
       const user = await this.usersRepository.findById(session.userId);
 
