fc3b0b05d8f16f8a2447664db7530ae51053e86a
Pablo Ferrari <pabloferraricaliari@gmail.com>
2025-11-30 16:26:50 -0300

    fix: update UserAlreadyExistsError to use HTTP 409 conflict

--- src/modules/users/domain/errors/user-already-exists.error.ts
+++ src/modules/users/domain/errors/user-already-exists.error.ts
@@ -2,6 +2,6 @@ import { CustomError } from '@/shared/common/custom-error';
 
 export class UserAlreadyExistsError extends CustomError {
   constructor(email: string) {
-    super(`User already exists: ${email}`, 400);
+    super(`User already exists: ${email}`, 409);
   }
 }
